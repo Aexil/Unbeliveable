035adf0b0efc3302ae54964c0727e32f7357c8e8
AEXIL <maxime.saidi.simplon@gmail.com>
2022-02-23 12:06:35 +0100

    JsPHP

---
+++ index.js
@@ -0,0 +1,9 @@
+$.get("./getProfil.php?name=technet")
+.done( function(data){
+	profil = jQuery.parseJSON(data);
+	$( "#result" ).text(profil.name );
+	$( "#LVL" ).text(profil.summonerLevel );
+	$( "#result" ).text(profil.name );
+	console.log(profil);
+	console.log(profil.name);
+});
