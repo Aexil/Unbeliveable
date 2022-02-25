afffb0374372301c95ba6d9a60bd08cd8d960deb
AEXIL <maxime.saidi.simplon@gmail.com>
2022-02-25 11:50:21 +0100

    Update JSPHP

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-$.get("./getProfil.php?name=technet")
+$.get("./getProfil.php?name=toto")
 .done( function(data){
-	profil = jQuery.parseJSON(data);
-	$( "#result" ).text(profil.name );
-	$( "#LVL" ).text(profil.summonerLevel );
-	$( "#result" ).text(profil.name );
-	console.log(profil);
-	console.log(profil.name);
+    profil = jQuery.parseJSON(data);
+    $( "#result" ).text(profil.name );
+    console.log(profil);
+    console.log(profil.profileIconId);
+    $("#sumIcon").attr('src', "http://ddragon.leagueoflegends.com/cdn/12.4.1/img/profileicon/"+profil.profileIconId+".png");
+    $("#result").text(profil.name);
+    $("#LVL").text(profil.summonerLevel);
+});
+
+$("#go").click( function(){
+	pseudo = $("#Joueur").val();
+	console.log("pseudo:"+pseudo);
 });
